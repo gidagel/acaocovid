ad8560386b7a7e0a1051bc369bb8f527fd6151cf
Fabio kanzato <giovanna@velabikes.com.br>
2021-01-19 14:22:52 -0300

    firebase hosting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
-import React from 'react'
-import { graphql, Link } from 'gatsby'
-import { RichText } from 'prismic-reactjs'
+import React, { useEffect } from 'react'
 import { withPreview } from 'gatsby-source-prismic'
 import Layout from '../components/layouts'
-import { ImageCaption, Quote, Text } from '../components/slices'
 
 
 export const Homepage = () => {
+
   return (
     <Layout>
       <a href='/blog'>Blog</a>
