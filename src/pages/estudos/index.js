a9ca4ef968a2e91ed867d176b327eafb15fe2d9c
gidagel <gidagel@gmail.com>
2021-03-28 00:58:27 -0300

    Gogogo

--- src/pages/estudos/index.js
+++ src/pages/estudos/index.js
@@ -61,7 +61,7 @@ export const query = graphql`
 
 // Using the queried Blog Home document data, we render the top section
 const StudiesHead = ({ study }) => {
-  const bgImage = page.image.localFile.publicURL
+  const bgImage = study.image.localFile.publicURL
   const avatar = { backgroundImage: `url(${bgImage})`, backgroundPosition: '50% 30%' }
   return (
     <div className="home-header" data-wio-id={study.id}>
