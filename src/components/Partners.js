9f1b71d8a43e812ec9d62878cf57eac4e50b4a28
gidagel <gidagel@gmail.com>
2021-07-27 16:10:06 +0200

    Insert partner

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PortalCovid from '../images/Portal-Covid-Brasil-Acao.jpg'
 import LASCOL from '../images/LASCOL-Acao-Covid.jpg'
 import FundTide from '../images/Fundacao-Tide-Setubal-Acao-Covid.jpg'
+import FundEg from '../images/Fundacao-Egydio-Setubal-Acao-Covid.jpg'
 import UFABC from '../images/UFABC-Acao-Covid.jpg'
 import ENAP from '../images/ENAP-Premio-Acao-Covid.jpg'
 import BNDES from '../images/BNDES-Premio-Acao-Covid.jpg'
@@ -32,6 +33,9 @@ const Partners = () => {
             <div className="partners-list">
               <div className="partner">
                 <a href="https://fundacaotidesetubal.org.br/" target="_blank"><img src={FundTide} alt="Fundação Tide Setubal"/></a>
+              </div>
+              <div className="partner">
+                <a href="https://fundacaojles.org.br/" target="_blank"><img src={FundEg} alt="Fundação Egydio Setubal"/></a>
               </div>
               <div className="partner">
                 <a href="http://www.ufabc.edu.br/" target="_blank"><img src={UFABC} alt="UFABC"/></a>
